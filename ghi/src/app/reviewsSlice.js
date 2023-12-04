2f8132b210fa5cea774389464f30fd43ba357126
spiderthatcodes <jessicaldickerson@outlook.com>
2023-12-04 09:40:26 -0700

    updated redux

--- ghi/src/app/reviewsSlice.js
+++ ghi/src/app/reviewsSlice.js
@@ -1,71 +1,56 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const reviewsApi = createApi({
-  reducerPath: "reviewsApi",
-  baseQuery: fetchBaseQuery({
-    baseUrl: process.env.REACT_APP_API_HOST,
-  }),
-  tagTypes: ["Review"],
-  endpoints: (builder) => ({
-    /*
-    Users ????
-
-    Reviews state: GLOBAL => Accessible to Orders. Once a review is submitted then
-    the option to 'add review' disappears from available options
-*/
-
-    createReview: builder.mutation({
-      query: (data) => ({
-        url: "/reviews",
-        body: data,
-        method: "post",
-      }),
-      invalidatesTags: ["Review"],
+    reducerPath: 'reviewsApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: process.env.REACT_APP_API_HOST,
     }),
+    tagTypes: ['Review'],
+    endpoints: (builder) => ({
+        createReview: builder.mutation({
+            query: (data) => ({
+                url: '/reviews',
+                body: data,
+                credentials: 'include',
+                method: 'post',
+            }),
+            invalidatesTags: ['Review'],
+        }),
 
-    getAllReviews: builder.query({
-      query: () => "/reviews",
-      providesTags: ["Review"],
-    }),
+        getAllReviews: builder.query({
+            query: () => '/reviews',
+            providesTags: ['Review'],
+        }),
 
-    getReviewById: builder.query({
-      query: (id) => `/reviews/${id}`,
-    }),
+        getReviewById: builder.query({
+            query: (id) => `/reviews/${id}`,
+        }),
 
-    updateReview: builder.mutation({
-      query: ({ id, data }) => ({
-        url: `/reviews/${id}`,
-        body: data,
-        // body: { data },
-        method: "put",
-      }),
-      invalidatesTags: ["Review"],
-    }),
+        updateReview: builder.mutation({
+            query: ({ id, data }) => ({
+                url: `/reviews/${id}`,
+                body: data,
+                credentials: 'include',
+                method: 'PUT',
+            }),
+            invalidatesTags: ['Review'],
+        }),
 
-    deleteReview: builder.mutation({
-      query: ({ id, data }) => ({
-        url: `/reviews/${id}`,
-        body: data,
-        // body: { data },
-        method: "DELETE",
-        credentials: "include",
-      }),
-      invalidatesTags: ["Review"],
-      //   invalidatesTags: (response, error, arg) => {
-      //     console.log({ response, error, arg });
-      //     return [
-      //       { type: "Review", id: id },
-      //       { type: "Review", id: arg.order_id },
-      //     ];
-      //   },
+        deleteReview: builder.mutation({
+            query: ({ id }) => ({
+                url: `/reviews/${id}`,
+                method: 'DELETE',
+                credentials: 'include',
+            }),
+            invalidatesTags: ['Review'],
+        }),
     }),
-  }),
 });
 
 export const {
-  useCreateReviewMutation,
-  useGetAllReviewsQuery,
-  useGetReviewByIdQuery,
-  useUpdateReviewMutation,
-  useDeleteReviewMutation,
+    useCreateReviewMutation,
+    useGetAllReviewsQuery,
+    useGetReviewByIdQuery,
+    useUpdateReviewMutation,
+    useDeleteReviewMutation,
 } = reviewsApi;
