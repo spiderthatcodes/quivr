49d498038ed35c31300739bda080dc8a02965805
Micheal <mikey4426@gmail.com>
2023-12-05 10:00:01 -0700

    background

--- ghi/src/pages/orderHistory/OrderHistory.jsx
+++ ghi/src/pages/orderHistory/OrderHistory.jsx
@@ -1,132 +1,85 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useGetAllOrdersQuery } from '../../app/ordersSlice';
-import { useGetTokenQuery } from '../../app/authSlice';
-import OrderRow from '../../components/orderRow/OrderRow';
-import OrderDetails from '../../components/orderDetails/OrderDetails';
-import { Wrapper, Table, LandBackground } from '../../constants';
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useGetAllOrdersQuery } from "../../app/ordersSlice";
+import { useGetTokenQuery } from "../../app/authSlice";
+import OrderRow from "../../components/orderRow/OrderRow";
+import OrderDetails from "../../components/orderDetails/OrderDetails";
+import { Wrapper, LandBackground, Table } from "../../constants";
 
 const OrderHistory = () => {
-    const navigate = useNavigate();
-    const { data: account, isLoading } = useGetTokenQuery();
-    const { data: allOrders, isLoading: ordersLoading } =
-        useGetAllOrdersQuery();
-    const [orders, setOrders] = useState([]);
-    const [showDetails, setShowDetails] = useState(false);
-    const [detailedOrder, setDetailedOrder] = useState({});
+  const navigate = useNavigate();
+  const { data: account, isLoading } = useGetTokenQuery();
+  const { data: allOrders, isLoading: ordersLoading } = useGetAllOrdersQuery();
+  const [orders, setOrders] = useState([]);
+  const [showDetails, setShowDetails] = useState(false);
+  const [detailedOrder, setDetailedOrder] = useState({});
 
-    useEffect(() => {
-        if (!isLoading && !account) {
-            navigate('/');
-        }
-        if (!ordersLoading && account?.role === 'customer') {
-            let list = allOrders.filter(
-                (item) => item.customer_id === account.username
-            );
-            setOrders(list);
-        } else if (!ordersLoading && account?.role === 'shaper') {
-            let list = allOrders.filter(
-                (item) => item.surfboard_shaper === account.username
-            );
-            setOrders(list);
-        } else {
-            !ordersLoading && setOrders(allOrders);
-        }
-    }, [account, allOrders, isLoading, navigate, ordersLoading]);
+  useEffect(() => {
+    if (!isLoading && !account) {
+      navigate("/");
+    }
+    if (!ordersLoading && account?.role === "customer") {
+      let list = allOrders.filter(
+        (item) => item.customer_id === account.username
+      );
+      setOrders(list);
+    } else if (!ordersLoading && account?.role === "shaper") {
+      let list = allOrders.filter(
+        (item) => item.surfboard_shaper === account.username
+      );
+      setOrders(list);
+    } else {
+      !ordersLoading && setOrders(allOrders);
+    }
+  }, [account, allOrders, isLoading, navigate, ordersLoading]);
 
-    if (isLoading || ordersLoading)
-        return (
-            <LandBackground>
-                <Wrapper>
-                    {account && (
-                        <>
-                            <OrderDetails
-                                order={detailedOrder}
-                                showDetails={showDetails}
-                                setShowDetails={setShowDetails}
-                            />
-                            <Table>
-                                <thead>
-                                    <tr>
-                                        <th>Order Date</th>
-                                        {account.role !== 'customer' && (
-                                            <th>Customer</th>
-                                        )}
-                                        {account.role !== 'shaper' && (
-                                            <th>Shaper</th>
-                                        )}
-                                        <th>Length</th>
-                                        <th>Width</th>
-                                        <th>Thinkness</th>
-                                        <th>Order Status</th>
-                                        <th />
-                                        <th />
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {orders.map((item, index) => (
-                                        <OrderRow
-                                            item={item}
-                                            role={account.role}
-                                            key={index}
-                                            status={item.order_status}
-                                            setShowDetails={setShowDetails}
-                                            setDetailedOrder={setDetailedOrder}
-                                        />
-                                    ))}
-                                </tbody>
-                            </Table>
-                        </>
-                    )}
-                </Wrapper>
-            </LandBackground>
-        );
-
+  if (isLoading || ordersLoading)
     return (
-        <LandBackground>
-            <Wrapper>
-                {account && (
-                    <>
-                        <OrderDetails
-                            order={detailedOrder}
-                            showDetails={showDetails}
-                            setShowDetails={setShowDetails}
-                        />
-                        <Table>
-                            <thead>
-                                <tr>
-                                    <th>Order Date</th>
-                                    {account.role !== 'customer' && (
-                                        <th>Customer</th>
-                                    )}
-                                    {account.role !== 'shaper' && (
-                                        <th>Shaper</th>
-                                    )}
-                                    <th>Length</th>
-                                    <th>Width</th>
-                                    <th>Thickness</th>
-                                    <th>Order Status</th>
-                                    <th />
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {orders.map((item, index) => (
-                                    <OrderRow
-                                        item={item}
-                                        role={account.role}
-                                        key={index}
-                                        status={item.order_status}
-                                        setShowDetails={setShowDetails}
-                                        setDetailedOrder={setDetailedOrder}
-                                    />
-                                ))}
-                            </tbody>
-                        </Table>
-                    </>
-                )}
-            </Wrapper>
-        </LandBackground>
+      <Wrapper>
+        <h1>Loading...</h1>
+      </Wrapper>
     );
+
+  return (
+      <LandBackground>
+        <>
+        {account && (
+          <>
+            <OrderDetails
+              order={detailedOrder}
+              showDetails={showDetails}
+              setShowDetails={setShowDetails}
+            />
+            <Table>
+              <thead>
+                <tr>
+                  <th>Order Date</th>
+                  {account.role !== "customer" && <th>Customer</th>}
+                  {account.role !== "shaper" && <th>Shaper</th>}
+                  <th>Length</th>
+                  <th>Width</th>
+                  <th>Thickness</th>
+                  <th>Order Status</th>
+                </tr>
+              </thead>
+              <tbody>
+                {orders.map((item, index) => (
+                  <OrderRow
+                    item={item}
+                    role={account.role}
+                    key={index}
+                    status={item.order_status}
+                    setShowDetails={setShowDetails}
+                    setDetailedOrder={setDetailedOrder}
+                  />
+                ))}
+              </tbody>
+            </Table>
+          </>
+        )}
+      </>
+    </LandBackground>
+  );
 };
 
 export default OrderHistory;
--- ghi/src/pages/userList/UserList.jsx
+++ ghi/src/pages/userList/UserList.jsx
@@ -1,73 +1,64 @@
-import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useGetAllAccountsQuery, useGetTokenQuery } from '../../app/authSlice';
-import UserRow from '../../components/userRow/UserRow';
-// import { Wrapper} from '../../constants';
-import { Table } from './style';
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useGetAllAccountsQuery, useGetTokenQuery } from "../../app/authSlice";
+import UserRow from "../../components/userRow/UserRow";
+import { LandBackground, Table } from "../../constants";
 
 const UserList = () => {
-    let { data: allUsers } = useGetAllAccountsQuery();
-    const { data: account, isLoading } = useGetTokenQuery();
-    const navigate = useNavigate();
-    const [userList, setUserList] = useState([]);
+  let { data: allUsers } = useGetAllAccountsQuery();
+  const { data: account, isLoading } = useGetTokenQuery();
+  const navigate = useNavigate();
+  const [userList, setUserList] = useState([]);
 
-    useEffect(() => {
-        if (!isLoading && !account) {
-            navigate('/');
-        }
-        if (!isLoading && account.role === 'customer') {
-            navigate('/create-order');
-        }
-        if (allUsers && !isLoading) {
-            if (account.role === 'shaper') {
-                let filtered = allUsers.filter(
-                    (item) => item.role === 'customer'
-                );
-                setUserList(filtered);
-            } else {
-                setUserList(allUsers);
-            }
-        }
-    }, [account, allUsers, isLoading, navigate]);
+  useEffect(() => {
+    if (!isLoading && !account) {
+      navigate("/");
+    }
+    if (!isLoading && account.role === "customer") {
+      navigate("/create-order");
+    }
+    if (allUsers && !isLoading) {
+      if (account.role === "shaper") {
+        let filtered = allUsers.filter((item) => item.role === "customer");
+        setUserList(filtered);
+      } else {
+        setUserList(allUsers);
+      }
+    }
+  }, [account, allUsers, isLoading, navigate]);
 
-    return (
-        // <Wrapper>
-        <>
-            {account && (
-                <Table>
-                    <thead>
-                        <tr>
-                            <th>
-                                {account.role === 'shaper'
-                                    ? 'Customer Name'
-                                    : 'User Name'}
-                            </th>
-                            {account.role === 'shaper' ? (
-                                <>
-                                    <th>Orders In Progress</th>
-                                    <th>Completed Orders</th>
-                                </>
-                            ) : (
-                                <th>User Type</th>
-                            )}
-                            <th>Email</th>
-                            <th>Phone Number</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {userList.map((item, index) => (
-                            <UserRow
-                                item={item}
-                                role={account.role}
-                                key={index}
-                            />
-                        ))}
-                    </tbody>
-                </Table>
-            )}
-        </>
-        // </Wrapper>
-    );
+  return (
+    <>
+      <LandBackground>
+        {account && (
+          <Table>
+            <thead>
+              <tr>
+                <th>
+                  {account.role === "shaper" ? "Customer Name" : "User Name"}
+                </th>
+                {account.role === "shaper" ? (
+                  <>
+                    <th>Orders In Progress</th>
+                    <th>Completed Orders</th>
+                  </>
+                ) : (
+                  <th>User Type</th>
+                )}
+                <th>Email</th>
+                <th>Phone Number</th>
+              </tr>
+            </thead>
+            <tbody>
+              {userList.map((item, index) => (
+                <UserRow item={item} role={account.role} key={index} />
+              ))}
+            </tbody>
+          </Table>
+        )}
+      </LandBackground>
+    </>
+  );
 };
 
 export default UserList;
