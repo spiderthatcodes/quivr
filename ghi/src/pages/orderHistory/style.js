472113f4f468b63f3dbc7d8fdb7e6549aa190167
pizzawarrior <i.norstad@gmail.com>
2023-12-05 08:20:37 -0800

    created style file for orderHistory, fixed backgoriund issue

---
+++ ghi/src/pages/orderHistory/style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import Background from "../../images/pexels-pixabay-390051.jpg";
+
+export const ImgBackground = styled.div`
+  background-image: url(${Background});
+  height: 60vh;
+  width: 100%;
+  background-repeat: no-repeat;
+  background-size: cover;
+  padding: 70px 0;
+`;
