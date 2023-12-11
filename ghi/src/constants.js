a2c8eb2e3cc7eb15030b3a54a8aac725e425e650
Ian Norstad <i.norstad@gmail.com>
2023-12-11 20:08:00 +0000

    Ian

--- ghi/src/constants.js
+++ ghi/src/constants.js
@@ -1,114 +1,114 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Wrapper = styled.div`
-    max-width: 1200px;
-    margin: auto;
-    padding: 12px;
-    min-height: 80vh;
+  max-width: 1200px;
+  margin: auto;
+  padding: 12px;
+  min-height: 80vh;
 `;
 
 export const Table = styled.table`
-    width: 100%;
-    max-width: 1200px;
-    margin: auto;
-    margin-bottom: 50px;
-    padding: 12px;
-    min-height: 60vh;
-    background-color: #105469;
+  width: 100%;
+  max-width: 1200px;
+  margin: auto;
+  margin-bottom: 50px;
+  padding: 12px;
+  min-height: 60vh;
+  background-color: #105469;
 
-    *,
-    *:before,
-    *:after {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-    }
+  *,
+  *:before,
+  *:after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
 
-    body {
-        background: #105469;
-        font-family: 'Open Sans', sans-serif;
-    }
-    table {
-        background: #e76215;
-        border-radius: 0.25em;
-        border-collapse: collapse;
-        margin: 1em;
-    }
-    th {
-        border-bottom: 1px solid #364043;
-        color: #e2b842;
-        font-size: 0.85em;
-        font-weight: 600;
-        padding: 0.5em 1em;
-        text-align: left;
-    }
-    td {
-        color: #fff;
-        font-weight: 400;
-        padding: 0.65em 1em;
-    }
-    .disabled td {
-        color: #4f5f64;
-    }
-    tbody tr {
-        transition: background 0.25s ease;
-    }
-    tbody tr:hover {
-        background: #e76215;
-    }
+  body {
+    background: #105469;
+    font-family: "Open Sans", sans-serif;
+  }
+  table {
+    background: #e76215;
+    border-radius: 0.25em;
+    border-collapse: collapse;
+    margin: 1em;
+  }
+  th {
+    border-bottom: 1px solid #364043;
+    color: #e2b842;
+    font-size: 0.85em;
+    font-weight: 600;
+    padding: 0.5em 1em;
+    text-align: left;
+  }
+  td {
+    color: #fff;
+    font-weight: 400;
+    padding: 0.65em 1em;
+  }
+  .disabled td {
+    color: #4f5f64;
+  }
+  tbody tr {
+    transition: background 0.25s ease;
+  }
+  tbody tr:hover {
+    background: #e76215;
+  }
 `;
 
 export const ModalContainer = styled.div`
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    -ms-transform: translate(-50%, -50%);
-    transform: translate(-50%, -50%);
-    background-color: #fff;
-    padding: 80px 100px;
-    border-radius: 8px;
-    border: 8px solid #df6b3a;
-    z-index: 1000;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+  background-color: #fff;
+  padding: 80px 100px;
+  border-radius: 8px;
+  border: 8px solid #df6b3a;
+  z-index: 1000;
 `;
 
 export const AccountForm = styled.div`
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    -ms-transform: translate(-50%, -50%);
-    transform: translate(-50%, -50%);
-    background-color: #0d5274;
-    color: #f7e7be;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+  background-color: #0d5274;
+  color: #f7e7be;
+  text-align: center;
+  align-items: center;
+  padding: 32px 80px;
+  border-radius: 8px;
+  text-align: left;
+  opacity: 0.9;
+
+  h1 {
     text-align: center;
-    align-items: center;
-    padding: 32px 80px;
-    border-radius: 8px;
-    text-align: left;
-    opacity: 0.75;
+  }
 
-    h1 {
-        text-align: center;
-    }
+  button {
+    margin: 18px 0;
+    background-color: rgb(253, 137, 64);
+    padding: 12px 24px;
+    color: #fff;
+    border-radius: 4px;
+    letter-spacing: 1px;
+    font-weight: bold;
+    border: none;
+  }
 
-    button {
-        margin: 18px 0;
-        background-color: rgb(253, 137, 64);
-        padding: 12px 24px;
-        color: #fff;
-        border-radius: 4px;
-        letter-spacing: 1px;
-        font-weight: bold;
-        border: none;
-    }
-
-    input {
-        width: 20vw;
-    }
+  input {
+    width: 20vw;
+  }
 `;
 
 export const OrderOptions = [
-    'Order Received',
-    'Foam being cut',
-    'Sent to glassing',
-    'Completed',
+  "Order Received",
+  "Foam being cut",
+  "Sent to glassing",
+  "Completed",
 ];
